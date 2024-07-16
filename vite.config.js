a19d687c562bd7d293804cd9bac35414d18284e4
sachin kumar <sk2105@yahoo.com>
2024-07-16 08:30:33 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sk/',
+  base: '/MyPortfolio/',
   plugins: [react()],
   build: {
-    outDir:"sk"
+    outDir:"dist"
   }
 })
