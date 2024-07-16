69d3f9a5c6cad27f2c78d8a7320e40d150f2beb8
sachin kumar <sk2105@yahoo.com>
2024-07-16 08:22:47 +0530

    second realease

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MyPortfolio/',
+  base: '/sk/',
   plugins: [react()],
   build: {
-    outDir:"MyPortfolio"
+    outDir:"sk"
   }
 })
